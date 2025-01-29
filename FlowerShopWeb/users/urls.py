3f552b8c980ce8a94d7b0dc912d15791f8c843ce
Mikhail Bukharov <mikhail.bukharov@gmail.com>
2025-01-29 15:35:15 +0300

    message to login for order

--- FlowerShopWeb/users/urls.py
+++ FlowerShopWeb/users/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
 ]
