0d7504871b31c5478ddae697a93a32f051269bc5
Mikhail Bukharov <mikhail.bukharov@gmail.com>
2025-01-29 16:12:24 +0300

    dd name and phone to customuser model, change register.html

--- FlowerShopWeb/FlowerShopWeb/settings.py
+++ FlowerShopWeb/FlowerShopWeb/settings.py
@@ -124,11 +124,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/catalog/",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- FlowerShopWeb/users/models.py
+++ FlowerShopWeb/users/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    name = models.CharField(max_length=100, verbose_name="Имя",default='')
+    phone = models.CharField(max_length=20, verbose_name="Телефон",default='')
     # add additional fields in here
 
 
